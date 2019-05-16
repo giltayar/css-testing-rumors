0f09b4fc9065170175d0124f03da8e4a7452eae6
Gil Tayar <gil@tayar.org>
2019-05-16 06:22:13 +0300

    fine tuning image-snapshot tests

--- cypress/integration/image-snapshot-simple-test.js
+++ cypress/integration/image-snapshot-simple-test.js
@@ -6,7 +6,6 @@ it('should test with image-snapshot-simple-test', () => {
   cy.viewport(1024, 768)
 
   cy.visit('http://localhost:3000')
-
   cy.get('#searchBar').type('test')
 
   cy.matchImageSnapshot('home-page')
