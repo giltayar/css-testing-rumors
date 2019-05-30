aed12471e306f0a9912c20708511c853e32d626c
Gil Tayar <gil@tayar.org>
2019-05-31 00:32:51 +0200

    fine tuning for css conf

--- cypress/integration/eyes-simple-test.js
+++ cypress/integration/eyes-simple-test.js
@@ -7,10 +7,19 @@ it('should test with eyes', () => {
   cy.visit('http://localhost:3000')
 
   cy.eyesOpen({
-    appName: 'Testing Bookstore',
-    batchName: 'Testing Bookstore',
+    appName: 'Testing at CSSConf',
+    batchName: 'Testing at CSSConf',
     testName: 'Home Page',
+    browser: [
+      {name: 'chrome', width: 1024, height: 768},
+      {name: 'chrome', width: 800, height: 600},
+      {name: 'chrome', width: 700, height: 600},
+      {deviceName: 'iPhone X'},
+      {name: 'ie', width: 800, height: 600},
+    ],
   })
+
+  cy.get('#searchBar').type('test')
 
   cy.eyesCheckWindow('home page')
 
